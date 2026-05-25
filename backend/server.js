3645b2bbe83bd5d2c013f0f8001bd0b7c5b7d090
Qiqi <qiqi8953@gmail.com>
2026-05-25 19:30:34 +0800

    Change nothing

--- backend/server.js
+++ backend/server.js
@@ -287,6 +287,8 @@ app.get('/health', (req, res) => {
 // =====================
 // SERVER
 // =====================
-app.listen(3000, () => {
-  console.log('Backend running at http://localhost:3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
 })
