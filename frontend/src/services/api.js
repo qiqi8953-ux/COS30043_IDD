ac642e0bac961f5dd0e9ea54e3f4da4cfad9b78b
Qiqi <qiqi8953@gmail.com>
2026-05-25 19:40:07 +0800

    change api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://cos30043-idd-backend.onrender.com'
+  baseURL: 'https://cos30043-idd-backend.onrender.com/api/hotels'
 })
